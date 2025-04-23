32c73756da9dcfc8162d1dea936734e42c8081fc
eitan-que <49550620@est.ort.edu.ar>
2025-04-23 15:38:51 -0300

    TP2UI

---
+++ Assets/CLAVEMANAGER.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class CLAVEMANAGER : MonoBehaviour
+{
+
+    public TextMeshProUGUI TextoMenesaje;
+    public TMP_InputField InputClave;
+    string clave = "tic2025";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        TextoMenesaje.text = string.Empty;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ComprobarClave()
+    {
+        string ClaveIngresada = InputClave.text;
+        if (ClaveIngresada != clave)
+        {
+            TextoMenesaje.text = "Acceso denegado";
+            InputClave.text = string.Empty;
+            return;
+        }
+
+        TextoMenesaje.text = "Bienvenido!";
+        return;
+    }
+}
